9a7391bac50d70f7f31c173cd31ee119895a2cdf
Christian Bryan <christian.bryan@christi-ltm3ydr.internal.salesforce.com>
2022-06-22 15:05:47 -0700

    steps 1-3

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,13 +1,25 @@
-import * as React from "react"
-import Tweet from "../Tweet/Tweet"
-import TweetBox from "../TweetBox/TweetBox"
-import "./Feed.css"
+import * as React from "react";
+import Tweet from "../Tweet/Tweet";
+import TweetBox from "../TweetBox/TweetBox";
+import "./Feed.css";
 
-export default function Feed(props) {
+export default function Feed({
+  tweets,
+  setTweets,
+  userProfile,
+  tweetText,
+  setTweetText,
+}) {
   return (
     <div className="col feed">
       {/* UPDATE TWEET BOX PROPS HERE */}
-      <TweetBox />
+      <TweetBox
+        tweets={tweets}
+        setTweets={setTweets}
+        userProfile={userProfile}
+        tweetText={tweetText}
+        setTweetText={setTweetText}
+      />
 
       <div className="see-new-tweets beet">
         <p>
@@ -15,7 +27,11 @@ export default function Feed(props) {
         </p>
       </div>
 
-      <div className="twitter-feed">{/* ADD CODE HERE */}</div>
+      <div className="twitter-feed">
+        {tweets.map((tweet, i) => (
+          <Tweet key={i} tweet={tweet} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
--- src/components/TweetBox/TweetBox.jsx
+++ src/components/TweetBox/TweetBox.jsx
@@ -1,19 +1,45 @@
-import * as React from "react"
-import TweetInput from "./TweetInput"
-import "./TweetBox.css"
+import * as React from "react";
+import TweetInput from "./TweetInput";
+import "./TweetBox.css";
 
-export default function TweetBox(props) {
+export default function TweetBox({
+  tweets,
+  setTweets,
+  userProfile,
+  tweetText,
+  setTweetText,
+}) {
+  function handleOnTweetTextChange(event) {
+    setTweetText(event.target.value);
+  }
+  function handleOnSubmit() {
+    const newTweet = {
+      name: userProfile.name,
+      handle: userProfile.handle,
+      text: "",
+      comments: 0,
+      retweets: 0,
+      likes: 0,
+      id: tweets.length,
+    };
+    setTweets(tweets.concat(newTweet));
+  }
   return (
     <div className="tweet-box">
-      <TweetInput />
+      <TweetInput
+        tweets={tweets}
+        tweetText={tweetText}
+        handleOnTweetTextChange={handleOnTweetTextChange}
+      />
+      {/* <TweetBox tweets={tweets} setTweets={setTweets} userProfile={userProfile} /> */}
 
       <div className="tweet-box-footer">
         <TweetBoxIcons />
         <TweetCharacterCount />
-        <TweetSubmitButton />
+        <TweetSubmitButton handleOnSubmit={handleOnSubmit} />
       </div>
     </div>
-  )
+  );
 }
 
 export function TweetBoxIcons() {
@@ -24,19 +50,21 @@ export function TweetBoxIcons() {
       <i className="far fa-chart-bar"></i>
       <i className="fas fa-map-marker-alt"></i>
     </div>
-  )
+  );
 }
 
 export function TweetCharacterCount(props) {
   // ADD CODE HERE
-  return <span></span>
+  return <span></span>;
 }
 
-export function TweetSubmitButton() {
+export function TweetSubmitButton({ handleOnSubmit }) {
   return (
     <div className="tweet-submit">
       <i className="fas fa-plus-circle"></i>
-      <button className="tweet-submit-button">Tweet</button>
+      <button className="tweet-submit-button" onClick={handleOnSubmit}>
+        Tweet
+      </button>
     </div>
-  )
+  );
 }
